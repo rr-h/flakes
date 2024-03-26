55bef43b1698fe9af54b8919e73a5665d140ab61
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-26 13:52:06 +0000

    Update networking.nix

--- system/networking.nix
+++ system/networking.nix
@@ -5,7 +5,7 @@
     enable = true;
     dns = "none";
   };
-  networking.hostName = "shinishiho"; # Define your hostname.
+  networking.hostName = "rr-h-nix"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
