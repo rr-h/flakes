05d7a0f1caf8a413969a92c8713f8884e3ad1643
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-26 13:57:36 +0000

    Update user.nix

--- system/user.nix
+++ system/user.nix
@@ -1,14 +1,28 @@
-{ pkgs, ... }:
-
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nakxkri = {
-    initialPassword = "1234";
+  # Define user-specific configurations
+  users.users.rr-h = {
     isNormalUser = true;
-    description = "ShiniShiho";
-    extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "tss" ];
-    shell = pkgs.zsh;
+    description = "Ricardo";
+    extraGroups = [ "networkmanager" "wheel" ]; # Additional groups
+    packages = with pkgs; [ vim wget ]; # User-specific packages
   };
 
-  users.users.root.hashedPassword = "!";
+  # Define sudo rules for the user
+  security.sudo.extraRules = [
+    {
+      groups = [ "wheel" ]; # Sudo group
+      commands = [
+        "${pkgs.systemd}/bin/systemctl suspend" # Suspend command
+        "${pkgs.systemd}/bin/reboot" # Reboot command
+        "${pkgs.systemd}/bin/poweroff" # Power off command
+        "/run/current-system/sw/bin/nixos-rebuild" # NixOS rebuild command
+        "${pkgs.neovim}/bin/nvim" # Neovim command
+        "${pkgs.systemd}/bin/systemctl" # Systemctl command
+        "/run/current-system/sw/bin/ln" # Symbolic link command
+        "/run/current-system/sw/bin/nix-channel" # Nix channel command
+      ];
+      options = [ "NOPASSWD" ]; # Options for sudo rules
+    }
+  ];
 }
+
