59ab492cf6054dc21a7f833701ce14ffbe248eaa
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-26 13:42:14 +0000

    Update boot.nix

--- system/boot.nix
+++ system/boot.nix
@@ -1,32 +1,33 @@
 { pkgs, ... }:
 
 {
-    boot = {
-        loader.systemd-boot.enable = true;
-        loader.efi.canTouchEfiVariables = true;
-        loader.efi.efiSysMountPoint = "/boot";
-        loader.timeout = 0;
-        initrd.enable = true;
-        initrd.systemd.enable = true;
-	initrd.verbose = false;
-	consoleLogLevel = 0;
-        plymouth = {
-            enable = true;
-            font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-            themePackages = [ pkgs.catppuccin-plymouth ];
-            theme = "catppuccin-macchiato";
-        };
-
-# Linux Kernel
-        kernelPackages = pkgs.linuxKernel.packages.linux_zen;
-        kernelParams = [ 
-            "quiet"
-	    "splash"
-	    "loglevel=3"
-	    "rd.udev.log_level=3"
-	    "rd.udev.log_priority=3"
-            "fbcon=nodefer"
-            "vt.global_cursor_default=0"
-        ];
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.efi.efiSysMountPoint = "/boot";
+    loader.timeout = 0;
+    initrd.enable = true;
+    initrd.systemd.enable = true;
+    initrd.verbose = false;
+    consoleLogLevel = 0;
+    plymouth = {
+      enable = true;
+      font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+      themePackages = [ pkgs.catppuccin-plymouth ];
+      theme = "catppuccin-macchiato";
     };
+
+    # Linux Kernel
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    kernelParams = [ 
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.udev.log_level=3"
+      "rd.udev.log_priority=3"
+      "fbcon=nodefer"
+      "vt.global_cursor_default=0"
+    ];
+  };
 }
+
