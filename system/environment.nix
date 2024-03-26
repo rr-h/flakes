8254c8dc706be0c9dda85d9582cc286a2947cae3
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-26 13:50:30 +0000

    Update environment.nix

--- system/environment.nix
+++ system/environment.nix
@@ -1,24 +1,30 @@
-{ pkgs, ... }:
-
 {
-    environment = {
+  environment = {
+    # Set GTK theme
+    variables.GTK_THEME = "Catppuccin-Macchiato-Standard-Teal-Dark";
 
-        variables.GTK_THEME = "Catppuccin-Macchiato-Standard-Teal-Dark";
-        variables.XCURSOR_THEME = "Catppuccin-Macchiato-Teal";
-        variables.XCURSOR_SIZE = "24";
-        variables.SPOTIFY_PATH = "${pkgs.spotify}/";
-        variables.JDK_PATH = "${pkgs.jdk11}/";
-        variables.NODEJS_PATH = "${pkgs.nodePackages_latest.nodejs}/";
+    # Set XCursor theme and size
+    variables.XCURSOR_THEME = "Catppuccin-Macchiato-Teal";
+    variables.XCURSOR_SIZE = "24";
 
-        variables.CI = "1";
-# variables.CLIPBOARD_EDITOR = "hx";
-        variables.CLIPBOARD_NOAUDIO = "1";
-# variables.CLIPBOARD_NOGUI = "1";
-# variables.CLIPBOARD_NOPROGRESS = "1";
-# variables.CLIPBOARD_NOREMOTE = "1";
-        variables.CLIPBOARD_SILENT = "1";
+    # Set paths for Spotify, JDK, and Node.js
+    variables.SPOTIFY_PATH = "${pkgs.spotify}/";
+    variables.JDK_PATH = "${pkgs.jdk11}/";
+    variables.NODEJS_PATH = "${pkgs.nodePackages_latest.nodejs}/";
 
-        sessionVariables.NIXOS_OZONE_WL = "1";
-        sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
-    };
+    # Enable Continuous Integration
+    variables.CI = "1";
+
+    # Clipboard settings
+    variables.CLIPBOARD_EDITOR = "hx";
+    variables.CLIPBOARD_NOAUDIO = "1";
+    variables.CLIPBOARD_NOGUI = "1";
+    variables.CLIPBOARD_NOPROGRESS = "1";
+    variables.CLIPBOARD_NOREMOTE = "1";
+    variables.CLIPBOARD_SILENT = "1";
+
+    # Set session variables for NixOS Ozone and Wayland
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  };
 }
